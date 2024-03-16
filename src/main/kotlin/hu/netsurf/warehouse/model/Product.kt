9b55eac49b9c4e1fc695f05101115b129fc9e428
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-16 18:45:11 +0100

    Add extra fields to Product.

--- src/main/kotlin/hu/netsurf/warehouse/model/Product.kt
+++ src/main/kotlin/hu/netsurf/warehouse/model/Product.kt
@@ -3,5 +3,9 @@ package hu.netsurf.warehouse.model
 data class Product(
     val id: Int,
     val name: String,
-    val supplier: Supplier
+    val supplier: Supplier,
+    val price: Double,
+    val unit: String,
+    val photo: String,
+    val onStock: Int,
 )
