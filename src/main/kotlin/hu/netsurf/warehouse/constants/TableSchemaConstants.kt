9034364e962aaab1a40818942cdf85884bacae0f
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-03-17 11:34:30 +0100

    Transform Product and Supplier models to JPA entities.

---
+++ src/main/kotlin/hu/netsurf/warehouse/constants/TableSchemaConstants.kt
@@ -0,0 +1,6 @@
+package hu.netsurf.warehouse.constants
+
+object TableSchemaConstants {
+    const val PRODUCTS_TABLE_NAME = "products"
+    const val SUPPLIERS_TABLE_NAME = "suppliers"
+}
--- src/main/kotlin/hu/netsurf/warehouse/model/Supplier.kt
+++ src/main/kotlin/hu/netsurf/warehouse/model/Supplier.kt
@@ -1,6 +1,18 @@
 package hu.netsurf.warehouse.model
 
+import hu.netsurf.warehouse.constants.TableSchemaConstants
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = TableSchemaConstants.SUPPLIERS_TABLE_NAME)
 data class Supplier(
-    val id: Int,
-    val name: String
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int = 0,
+
+    var name: String = "",
 )
